d95fa0df3b0037ba9148519fc196466e3058bab3
Subhanshu Bansal <subhashu20135@iiitd.ac.in>
2023-03-07 18:14:59 +0530

    Added JS Methods

--- src/main/java/com/example/springbootdemo/controller/HomeController.java
+++ src/main/java/com/example/springbootdemo/controller/HomeController.java
@@ -1,8 +1,8 @@
 package com.example.springbootdemo.controller;
 
+import com.example.springbootdemo.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
@@ -11,5 +11,20 @@ public class HomeController {
     @RequestMapping("/")
     public String home() {
         return "Hello World";
+    }
+
+    @GetMapping("/user")
+    public User _user() {
+        User user = new User();
+        user.setId("1");
+        user.setName("Subhanshu Bansal");
+        user.setEmailId("subhanshu.bansal5566@gmail.com");
+
+        return user;
+    }
+
+    @GetMapping("/{id}")
+    public String pathVariable(@PathVariable String id) {
+        return "The path variable is:" + id;
     }
 }
